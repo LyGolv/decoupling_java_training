4f452e65757ab7e137639621176fd03d8b5bfe13
Guimdo Lioguy <vianel.lioguy@gmail.com>
2022-10-06 10:56:01 +0200

    Setup Log Management

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,10 @@
+package fr.lernejo.logger;
+
+public class LoggerFactory {
+
+    private static final Logger logger = null;
+
+    public static Logger getLogger(String name) {
+        return logger;
+    }
+}
